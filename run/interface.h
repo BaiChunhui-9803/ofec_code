f20ac50c71ddea2e7749ce958fd76e86e9a84ace
binich-9803 <bch980311@163.com>
2025-02-15 20:57:03 +0800

    Add comments to interface.h

--- run/interface.h
+++ run/interface.h
@@ -9,17 +9,51 @@
 #include "factory.h"
 
 namespace ofec {
+	// Factory class for creating Environment objects
 	extern Factory<Environment> g_factory_environment;
+	// Factory class for creating Problem objects
 	extern Factory<Problem> g_factory_problem;
+	// Factory class for creating Algorithm objects
 	extern Factory<Algorithm> g_factory_algorithm;
+	// Mapping table storing the relationship between environment problems and supported algorithms
 	extern std::map<std::string, std::set<std::string>> g_algorithm_for_environment_problem;
 	void registerInstance();
+
+	/**
+ 	 * @brief_byBCH Check if a specific environment problem supports a given algorithm.
+ 	 *
+ 	 * @param environment_problem_name The name of the environment problem.
+ 	 * @param algorithm_name The name of the algorithm.
+ 	 * @return true If the environment problem supports the algorithm.
+ 	 * @return false If the environment problem does not support the algorithm.
+ 	 */
 	bool checkValidation(
 		const std::string &environment_problem_name, 
 		const std::string &algorithm_name
 	);
+
+	/**
+ 	 * @brief_byBCH Generate an Environment object based on the environment problem name.
+ 	 *
+ 	 * @param environment_problem_name The name of the environment problem.
+ 	 * @return Environment* Pointer to the created Environment object.
+ 	 */
 	Environment* generateEnvironmentByFactory(const std::string &environment_problem_name);
+
+	/**
+  	 * @brief_byBCH Generate a Problem object based on the environment problem name.
+  	 *
+  	 * @param environment_problem_name The name of the environment problem.
+  	 * @return Problem* Pointer to the created Problem object.
+  	 */
 	Problem* generateProblemByFactory(const std::string &environment_problem_name);
+
+	/**
+  	 * @brief_byBCH Generate an Algorithm object based on the algorithm name.
+  	 *
+  	 * @param algorithm_name The name of the algorithm.
+  	 * @return Algorithm* Pointer to the created Algorithm object.
+  	 */
 	Algorithm* generateAlgorithmByFactory(const std::string &algorithm_name);
 }
 
