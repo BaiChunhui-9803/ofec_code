c1cca4b4c13ce5697383ad7d25ce49f80045dcae
binich-9803 <bch980311@163.com>
2025-02-17 16:26:58 +0800

    Add comments to parameter_map.h

--- core/parameter/parameter_map.h
+++ core/parameter/parameter_map.h
@@ -24,6 +24,14 @@
 #include <ostream>
 
 namespace ofec {
+	/**
+ 	* @class ParameterMap
+ 	* @brief_byBCH A class for managing a collection of parameters with various types.
+ 	*
+ 	* This class provides a flexible way to store, retrieve, and manage parameters
+ 	* of different types using a unified interface. It is designed to support
+ 	* dynamic parameter handling in evolutionary computation frameworks.
+ 	*/
 	class ParameterMap {
 	private:
 		std::map<std::string, ParameterVariant> m_map;
@@ -36,19 +44,62 @@ namespace ofec {
 
 		virtual ~ParameterMap() = default;
 		friend bool operator==(const ParameterMap& p1, const ParameterMap& p2);
+
+		/**
+ 		* @brief_byBCH Overloaded operator[] to access a parameter by key.
+ 		* @param key The key of the parameter to access.
+ 		* @return A reference to the ParameterVariant associated with the key.
+ 		*/
 		ParameterVariant& operator[](const std::string &key);
 		const ParameterVariant& at(const std::string &key) const;
+
+		/**
+ 		* @brief_byBCH Check if a parameter with the given key exists.
+ 		* @param key The key to check.
+ 		* @return True if the key exists, false otherwise.
+ 		*/
 		bool has(const std::string &key) const;
 		size_t erase(const std::string &key);
+
+		/**
+ 		* @brief_byBCH Clear all parameters from the map.
+ 		*/
 		void clear();
+
+		/**
+ 		* @brief_byBCH Get an iterator to the beginning of the map.
+ 		* @return A const iterator to the beginning of the map.
+ 		*/
 		CIteratorType begin() const { return m_map.begin(); }
+
+ 		/**
+ 		* @brief_byBCH Get an iterator to the end of the map.
+ 		* @return A const iterator to the end of the map.
+ 		*/
 		CIteratorType end() const { return m_map.end(); }
+		/**
+ 		* @brief_byBCH Get a const iterator to the beginning of the map.
+ 		* @return A const iterator to the beginning of the map.
+ 		*/
 		CIteratorType cbegin() const { return m_map.cbegin(); }
+
+		/**
+ 		* @brief_byBCH Get a const iterator to the end of the map.
+ 		* @return A const iterator to the end of the map.
+ 		*/
 		CIteratorType cend() const { return m_map.cend(); }
 
+		/**
+ 		* @brief_byBCH Get a parameter value by key, with type checking.
+ 		* @tparam T The expected type of the parameter.
+ 		* @param key The key of the parameter to retrieve.
+ 		* @return A const reference to the parameter value of type T.
+ 		* @throws Exception if the key does not exist or type mismatch occurs.
+ 		*/
 		template <typename T>
 		const T& get(const std::string &key) const {
 			if (m_map.count(key) > 0) {
+				auto a = m_map.at(key);
 				return std::get<T>(m_map.at(key));
 			}
 			else {
@@ -56,6 +107,13 @@ namespace ofec {
 			}
 		}
 
+		/**
+ 		* @brief_byBCH Get a parameter value by key, with type checking and default value.
+ 		* @tparam T The expected type of the parameter.
+ 		* @param key The key of the parameter to retrieve.
+ 		* @param default_val The default value to return if the key does not exist.
+ 		* @return A const reference to the parameter value of type T, or the default value.
+ 		*/
 		template <typename T>
 		const T& get(const std::string& key, const T &default_val) const {
 			if (m_map.count(key) > 0) {
@@ -67,7 +125,13 @@ namespace ofec {
 		}
 	};
 
-
+	/**
+	 * @brief_byBCH Convert a vector to a series of parameters in a ParameterMap.
+	 * @tparam T The type of elements in the vector.
+	 * @param v The ParameterMap to store the parameters.
+	 * @param name The base name for the parameters.
+	 * @param vt The vector to convert.
+	 */
 	template< typename T>
 	inline void vecToParam(ParameterMap &v, const std::string &name, const std::vector<T> &vt) {
 		v[name + char(2) + "size"] = int(vt.size());
@@ -76,6 +140,13 @@ namespace ofec {
 		}
 	}
 
+	/**
+ 	* @brief_byBCH Convert a series of parameters in a ParameterMap to a vector.
+ 	* @tparam T The type of elements in the resulting vector.
+ 	* @param v The ParameterMap containing the parameters.
+ 	* @param name The base name for the parameters.
+ 	* @return A vector containing the parameter values.
+ 	*/
 	template< typename T>
 	inline std::vector<T> paramToVec(const ParameterMap &v, const std::string &name) {
 		std::vector<T> vt(v.get<int>(name + char(2) + "size"));
